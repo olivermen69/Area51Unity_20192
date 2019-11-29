1c71b600777318b917f0305d8c88d0e2673ff7e4
olivermen69 <olivermen69@hotmail.com>
2019-11-28 21:57:24 -0500

    HangMan

---
+++ 20191128_MyHangManGame/20191128_MyHangManGame/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20191128_MyHangManGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            docFunction();
+        }
+
+        static void docFunction()
+        {
+            string secretWord = "zapatos";
+            string publicWord = "";
+            string tempWord = "";
+            string letter = "";
+            int hp = 3;
+
+            for (int i = 0; i < secretWord.Length; i++) {
+                tempWord += "*";
+            }
+            publicWord = tempWord;
+            Console.WriteLine(publicWord);
+
+            while (hp > 0) {
+                Console.WriteLine("HP:" + hp);
+                Console.WriteLine("Ingrese una letra:");
+                letter = Console.ReadLine();
+
+                Console.Clear();
+                // Console.ReadLine(">"+letter);
+
+                tempWord = "";
+                //foreach (char c in secretWord)
+                //{
+                //    if (letter == "" + c)
+                //    {
+                //        tempWord += c; //tempWord = tempWord + c;
+                //    }
+                //    else
+                //    {
+                //        tempWord += "*";
+                //    }
+                //}
+
+                for (int i = 0; i < secretWord.Length; i++)
+                {
+                    if (letter == "" + secretWord[i])
+                    {
+                        tempWord += letter;
+                    }
+                    else
+                    {
+                        tempWord += publicWord[i];
+                        //if (publicWord[i] != '*') tempWord += publicWord[i];
+                        //else tempWord += "*";
+                    }
+                }
+                if (publicWord == tempWord) { hp--; }
+
+                if (secretWord == tempWord)
+                {
+                    Console.WriteLine(tempWord);
+                    Console.WriteLine("YOU WIN!!");
+                    break;
+                }
+
+                publicWord = tempWord;
+                Console.WriteLine(publicWord);
+            }
+            //if (secretWord != tempWord)
+            if (hp <= 0)
+            {
+                Console.WriteLine("GAME OVER");
+            }
+            Console.ReadLine();
+        }
+
+        static void myFunction()
+        {
+            //string secretWord = "zapato";
+            string secretWord = "";
+            string letter = "";
+            string player = "";
+            int hp = 3;
+
+            Console.WriteLine("Hola, bienvenido a HangMan, cual es tu nombre? ");
+            player = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine(string.Format("Bienvenido {0}, empezemos el juego, ingresa una palabra:", player));
+            secretWord = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine(string.Format("La cantidad de vida es : {0}", hp));
+            Console.WriteLine(string.Format("La palabra elegida es : {0}", secretWord));
+            for (int i=0;i< secretWord.Length;i++) Console.Write(" _ ");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Ingrese una letra que corresponde a la palabra");
+            letter = Console.ReadLine();
+
+            if (myExisteLetraEnPalabra(secretWord, letter))
+            {
+                Console.WriteLine(string.Format("La cantidad de vida es : {0}", hp));
+                Console.WriteLine(string.Format("La palabra elegida es : {0}", secretWord));
+                for (int i = 0; i < secretWord.Length; i++)
+                {
+                    Console.Write(secretWord[i] == letter[0] ? " " + letter + " " : " _ ");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine("Ingrese una letra que corresponde a la palabra");
+                letter = Console.ReadLine();
+            }
+            else {
+                hp--;
+                myInitGame(hp, secretWord);
+                Console.WriteLine("Ingrese una letra que corresponde a la palabra");
+                letter = Console.ReadLine();
+            }
+            /*For...*/
+
+            //Console.WriteLine(">" + letter);
+
+            Console.ReadLine();
+
+        }
+         
+        static void myInitGame(int hp, string secretWord) {
+            Console.WriteLine(string.Format("La cantidad de vida es : {0}", hp));
+            Console.WriteLine(string.Format("La palabra elegida es : {0}", secretWord));
+            for (int i = 0; i < secretWord.Length; i++) Console.Write(" _ ");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static bool myExisteLetraEnPalabra(string palabra, string letra) {
+            bool existe = false;
+            for (int i = 0; i < palabra.Length; i++) {
+                if (palabra[i] == letra[0]) { existe = true; break; }
+            }
+            return existe;
+        }
+    }
+}
